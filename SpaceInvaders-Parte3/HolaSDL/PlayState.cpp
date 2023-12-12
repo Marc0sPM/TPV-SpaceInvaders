18787bf630d58280443189d6d3fd67684486e0db
Mikeey84 <milope21@ucm.es>
2023-12-12 18:37:21 +0100

    de momento esto

--- SpaceInvaders-Parte3/HolaSDL/PlayState.cpp
+++ SpaceInvaders-Parte3/HolaSDL/PlayState.cpp
@@ -1,7 +1,15 @@
 #include "PlayState.h"
 void PlayState::update() {
-
+	for (auto& it : sceneObjects) {
+		it.update();
+	}
 }
 void PlayState::render() const {
+	for (auto& it : sceneObjects) {
+		it.render();
+	}
+}
 
+int PlayState::getRandomRange(int min, int max) {
+	//return std::uniform_int_distribution<int>(min, max)(randomGenerator);
 }
--- SpaceInvaders-Parte3/HolaSDL/PlayState.h
+++ SpaceInvaders-Parte3/HolaSDL/PlayState.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "SDL.h"
+#include <list>
+#include "SceneObject.h"
 class PlayState
 {
 private:
@@ -6,5 +9,9 @@ private:
 public:
 	virtual void update();
 	virtual void render() const;
+	bool damage(SDL_Rect* laserRect, char& src);
+	void hasDied(std::list<SceneObject*>::iterator& iterator);
+	int getRandomRange(int min, int max);
+
 };
 
